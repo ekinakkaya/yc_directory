23eec66780e1a7a57301ca3a79f7662683eab7b1
Ekin Akkaya <ekoeko0654@gmail.com>
2024-11-24 00:27:38 +0300

    search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,26 @@
-const Home = () => {
+import SearchForm from "../components/SearchForm";
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query
   return (
     <>
-    <p className="heading">Home</p>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query}></SearchForm>
+      </section>
     </>
   );
-}
+};
 
 export default Home;
