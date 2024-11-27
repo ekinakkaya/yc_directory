385400a59bcb1660fbc3365c24df8c9be23eede6
Ekin Akkaya <ekoeko0654@gmail.com>
2024-11-27 21:37:57 +0300

    startup card ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "../components/StartupCard";
 
 const Home = async ({
   searchParams,
@@ -6,6 +7,18 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const query = (await searchParams).query
+
+  const posts = [
+    {
+      _createdAd: new Date(),
+      views: 55,
+      author: {_id: 1, name: "ekin"},
+      description: "this is a description.",
+      image: "https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png",
+      category: "Robots",
+      title: "We Robots",
+    }
+  ]
   return (
     <>
       <section className="pink_container">
@@ -18,6 +31,23 @@ const Home = async ({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query}></SearchForm>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"`: "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) =>(
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found.</p>
+          )}
+        </ul>
+
       </section>
     </>
   );
