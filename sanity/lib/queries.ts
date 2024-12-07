7ecbb721c3ffe7e5f8f67c4417deb9c75e66d7b7
Ekin Akkaya <ekoeko0654@gmail.com>
2024-12-07 21:12:31 +0300

    startup details page

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -32,3 +32,11 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`
   image
   }
   `);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(
+  `
+  *[_type == "startup" && _id == $id][0] {
+    _id, views
+  }
+  `
+)
