f44fc3a303dcb7b0ba73203423e022e30b1501ff
Ekin Akkaya <ekoeko0654@gmail.com>
2024-12-07 18:37:46 +0300

    caching, sanity live api

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,17 +2,18 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupCardType } from "../components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 const Home = async ({
   searchParams,
 }: {
   searchParams: Promise<{ query?: string }>;
 }) => {
-  const query = (await searchParams).query
+  const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
-  console.log(JSON.stringify(posts, null, 2))
+  console.log(JSON.stringify(posts, null, 2));
 
   return (
     <>
@@ -30,20 +31,21 @@ const Home = async ({
 
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for "${query}"`: "All Startups"}
+          {query ? `Search results for "${query}"` : "All Startups"}
         </p>
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType) =>(
+            posts.map((post: StartupCardType) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
             <p className="no-results">No startups found.</p>
           )}
         </ul>
-
       </section>
+
+      <SanityLive />
     </>
   );
 };
