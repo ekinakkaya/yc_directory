a66f1c9c47c3491954487322b76b3c831139bba7
Ekin Akkaya <ekoeko0654@gmail.com>
2024-12-07 18:47:01 +0300

    search by title, category, author

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,8 +10,9 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   console.log(JSON.stringify(posts, null, 2));
 
