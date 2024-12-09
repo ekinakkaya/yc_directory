871d571880b460acd47e013a9789628efea74b36
Ekin Akkaya <ekoeko0654@gmail.com>
2024-12-10 01:44:41 +0300

    parallel fetching

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -19,13 +19,13 @@ const md = markdownit();
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
-  const playlistResult = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-    slug: "editor-picks-0",
-  });
-
-  const editorPosts = playlistResult?.select || [];
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-0",
+    }),
+  ]);
 
   if (!post) return notFound();
 
