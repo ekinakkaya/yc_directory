ae4a16b997bbe2b40bde71ca2e5612fc6c230e77
Ekin Akkaya <ekoeko0654@gmail.com>
2024-12-09 19:36:24 +0300

    create startup server action implemented fully.

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -10,7 +10,8 @@ import { useActionState } from "react";
 import { formSchema } from "@/lib/validation";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -19,7 +20,7 @@ const StartupForm = () => {
 
   const { toast } = useToast();
 
-  //const router = useRouter();
+  const router = useRouter();
 
   const handleFormSubmit = async (prevState: any, formData: FormData) => {
     try {
@@ -33,15 +34,15 @@ const StartupForm = () => {
 
       await formSchema.parseAsync(formValues);
       console.log(formValues);
-      //const result = await createIdea(prevState, formData, pitch);
+      const result = await createPitch(prevState, formData, pitch);
 
-      //if (result.status == "SUCCESS") {
-      //  toast({
-      //    title: "Success",
-      //    description: "Your startup pitch has been successfully.",
-      //  });
-      //  router.push(`/startup/${result.id}`);
-      //}
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully.",
+        });
+        router.push(`/startup/${result.id}`);
+      }
 
     } catch (error) {
       if (error instanceof z.ZodError) {
